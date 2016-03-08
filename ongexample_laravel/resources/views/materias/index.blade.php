1ed403860323cc3309dc45474bdb015109992a8c
gkatzb <katz.gabriela@gmail.com>
2016-03-08 03:06:03 -0300

    Materias and Login views web and responsive design

--- ongexample_laravel/resources/views/materias/index.blade.php
+++ ongexample_laravel/resources/views/materias/index.blade.php
@@ -4,7 +4,7 @@
 	<div class="page-title wrapper disciplina-title">
 		<span>Escolha a disciplina:</span>
 	</div>
-	<div class="wrapper-materias">
+	<div class="container-fluid materias-container">
 		<div class="col-sm-12 col-xs-12">
 			<div class="col-sm-6 col-xs-6">
 				{!! Html::image('/assets/img/portugues.png', 'ONG', array('class' => 'btn materia-img')) !!}

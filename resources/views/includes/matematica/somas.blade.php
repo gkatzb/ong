155d9e1a7f9db1de77a9ac9995c9068dc835d97a
gkatzb <katz.gabriela@gmail.com>
2016-05-02 03:17:13 -0300

    sum

--- resources/views/includes/matematica/somas.blade.php
+++ resources/views/includes/matematica/somas.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="col-sm-6 col-xs-6">
             &nbsp;<span class="span-sum">=</span>&nbsp;
-            <input class="7 resp-soma numeric" maxlength="3" />
+            <input class="7 resp-soma numeric" maxlength="3" id="7" />
         </div>
         <div class="check-img col-sm-1 col-xs-1">
             <i class="hidden icon-atv-check icon-atv-check-7 fa fa-check fa-3x margin-bottom"></i>
@@ -14,14 +14,18 @@
         </div>
     </div>
     <div class="div-soma col-sm-12 col-xs-12">
+        <div class="check-img col-sm-1 col-xs-1">
+            <i class="hidden icon-atv-check icon-atv-check-8 fa fa-check fa-3x margin-bottom"></i>
+            <i class="hidden icon-atv-error icon-atv-error-8 fa fa-times fa-3x margin-bottom"></i>
+        </div>
         <div class="inner-sum col-sm-6 col-xs-6">
             &nbsp;
-            <input class="7 input-soma numeric" maxlength="3"/>&nbsp;<span class="span-sum-symbol">+</span>
-            <input class="7 input-soma numeric" maxlength="3"/>
+            <input class="3 input-soma input-soma1 numeric" maxlength="3" id="3" />&nbsp;<span class="span-sum-symbol">+</span>
+            <input class="4 input-soma input-soma2 numeric" maxlength="3" id="4" />
         </div>
         <div class="col-sm-6 col-xs-6">
             &nbsp;<span class="span-sum">=</span>&nbsp;
-            <input class="7 resp-soma numeric" maxlength="3" />
+            <input class="7 resp-soma numeric" maxlength="3" id="7" />
         </div>
         <div class="check-img col-sm-1 col-xs-1">
             <i class="hidden icon-atv-check icon-atv-check-7 fa fa-check fa-3x margin-bottom"></i>
@@ -35,7 +39,7 @@
         </div>
         <div class="col-sm-6 col-xs-6">
             &nbsp;<span class="span-sum">=</span>&nbsp;
-            <input class="9 resp-soma numeric" maxlength="3" />
+            <input class="9 resp-soma numeric" maxlength="3" id="9" />
         </div>
         <div class="check-img col-sm-1 col-xs-1">
             <i class="hidden icon-atv-check icon-atv-check-9 fa fa-check fa-3x margin-bottom"></i>
@@ -43,14 +47,18 @@
         </div>
     </div>
     <div class="div-soma col-sm-12 col-xs-12">
+        <div class="check-img col-sm-1 col-xs-1">
+            <i class="hidden icon-atv-check icon-atv-check-8 fa fa-check fa-3x margin-bottom"></i>
+            <i class="hidden icon-atv-error icon-atv-error-8 fa fa-times fa-3x margin-bottom"></i>
+        </div>
         <div class="inner-sum col-sm-6 col-xs-6">
             &nbsp;
-            <input class="9 input-soma numeric" maxlength="3"/>&nbsp;<span class="span-sum-symbol">+</span>
-            <input class="9 input-soma numeric" maxlength="3"/>
+            <input class="6 input-soma input-soma1 numeric" maxlength="3" id="6" />&nbsp;<span class="span-sum-symbol">+</span>
+            <input class="3 input-soma input-soma2 numeric" maxlength="3" id="3" />
         </div>
         <div class="col-sm-6 col-xs-6">
             &nbsp;<span class="span-sum">=</span>&nbsp;
-            <input class="9 resp-soma numeric" maxlength="3" />
+            <input class="9 resp-soma numeric" maxlength="3" id="9" />
         </div>
         <div class="check-img col-sm-1 col-xs-1">
             <i class="hidden icon-atv-check icon-atv-check-9 fa fa-check fa-3x margin-bottom"></i>
@@ -64,7 +72,7 @@
         </div>
         <div class="col-sm-6 col-xs-6">
             &nbsp;<span class="span-sum">=</span>&nbsp;
-            <input class="8 resp-soma numeric" maxlength="3" />
+            <input class="8 resp-soma numeric" maxlength="3" id="8" />
         </div>
         <div class="check-img col-sm-1 col-xs-1">
             <i class="hidden icon-atv-check icon-atv-check-8 fa fa-check fa-3x margin-bottom"></i>
@@ -72,14 +80,18 @@
         </div>
     </div>
     <div class="div-soma col-sm-12 col-xs-12">
+        <div class="check-img col-sm-1 col-xs-1">
+            <i class="hidden icon-atv-check icon-atv-check-8 fa fa-check fa-3x margin-bottom"></i>
+            <i class="hidden icon-atv-error icon-atv-error-8 fa fa-times fa-3x margin-bottom"></i>
+        </div>
         <div class="inner-sum col-sm-6 col-xs-6">
             &nbsp;
-            <input class="8 input-soma numeric" maxlength="3"/>&nbsp;<span class="span-sum-symbol">+</span>
-            <input class="8 input-soma numeric" maxlength="3"/>
+            <input class="4 input-soma input-soma1 numeric" maxlength="3" id="4" />&nbsp;<span class="span-sum-symbol">+</span>
+            <input class="4 input-soma input-soma2 numeric" maxlength="3" id="4"/>
         </div>
         <div class="col-sm-6 col-xs-6">
             &nbsp;<span class="span-sum">=</span>&nbsp;
-            <input class="8 resp-soma numeric" maxlength="3" />
+            <input class="8 resp-soma numeric" maxlength="3" id="8" />
         </div>
         <div class="check-img col-sm-1 col-xs-1">
             <i class="hidden icon-atv-check icon-atv-check-8 fa fa-check fa-3x margin-bottom"></i>
@@ -90,6 +102,7 @@
 @include('includes.scripts')
 <script>
     $(document).ready(function () {
+
         $('.numeric').keydown(function (e) {
             if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
                     (e.keyCode == 65 && e.ctrlKey === true) ||
@@ -103,96 +116,86 @@
             }
         });
 
-        var erros = 0;
-        var acertos = 0;
-        var qtd = 1;
-        var dateIni = getDate();
-        var clickNum = '';
-
-        $('.img-qtd-pic').each(function(){
-            if($(this).attr('class').split(' ')[2].split('-')[1] != qtd){
-                $(this).addClass('no-click');
-                $(this).attr('disabled', 'true');
-            }
-        });
-
-        function num_click(e){
-            clickNum = $(e).attr('class').split(' ')[1].split('-')[1];
-
-            if(!$(e).hasClass('no-click')){
-                $(e).addClass('no-click');
-                if (clickNum == qtd) {
-                    $(e).addClass('acerto');
-                    swal("Parabéns, resposta certa!", "", "success");
-                    playSound('claps');
-                    $('.icon-atv-check-'+qtd).removeClass('hidden');
-
-                    if(acertos < 6){
-                        $("#acertos-3").val(acertos++ + 1);
-
-                        $('.img-qtd-num').each(function(e){
-                            if(!($(this).hasClass('acerto'))){
-                                $(this).removeClass('no-click');
-                                $(this).removeAttr('disabled');
-                            }
-                        });
-
-                        $('.qtd-'+qtd).addClass('no-click');
-                        $('.qtd-'+qtd).attr('disabled', 'true');
-
-                        qtd++ + 1;
-
-                        $('.img-qtd-pic').each(function(){
-                            if($(this).attr('class').split(' ')[2].split('-')[1] != qtd){
-                                $(this).addClass('no-click');
-                                $(this).attr('disabled', 'true');
-                            } else if($(this).attr('class').split(' ')[2].split('-')[1] == qtd){
-                                $(this).removeClass('no-click');
-                                $(this).removeAttr('disabled');
-                            }
-                        });
-                    } else {
-
-                        $('.img-qtd-pic').each(function(){
-                            $(this).addClass('no-click');
-                            $(this).attr('disabled', 'true');
-                        });
-
-                        $('.img-qtd-num').each(function(){
-                            $(this).addClass('no-click');
-                            $(this).attr('disabled');
-                        });
-                    }
-                } else if (clickNum != qtd){
-                    $("#erros-3").val(erros++ + 1);
-                    swal("Ops... Resposta errada!", "", "warning");
-                    playSound('wrong');
-                }
-            }
-        }
-
-        $('.img-qtd-num').on('click', function () {
-            num_click(this);
-        });
-
         $('.btn-atividade-3').on('click', function (e) {
-            $().each(function(){
-                if
-            });
 
-            var sbtvId = $("#hdn_subatividade_id-3").val();
-            var toHide = '';
-            var toShow = '';
-            var erros = $("#erros-3").val();
-            var acertos = $("#acertos-3").val();
-            var dateFim = getDate();
-            var no_errors = false;
+            var erros = 0;
+            var acertos = 0;
+            var dateIni = getDate();
 
-            if(erros == 0){
-                no_errors = true;
+            if(acertos < 12) {
+                swal("Ops... Alguma resposta está errada!", "", "warning");
+                playSound('wrong');
+
+                $('.resp-soma').each(function(){
+                    var sum = $(this).attr('id');
+                    if($(this).val() == sum){
+                        $("#acertos-3").val(acertos++ + 1);
+                        $(this).addClass('no-click');
+                        $(this).attr('readonly', 'true');
+                        $(this).unbind('click');
+                        $(this).addClass('acerto');
+                        $(this).parent('div').next('div').find('.icon-atv-error').addClass('hidden');
+                        $(this).parent('div').next('div').find('.icon-atv-check').removeClass('hidden');
+                    } else if($(this).val() != sum){
+                        $("#erros-3").val(erros++ + 1);
+                        $(this).addClass('erro');
+                        $(this).parent('div').next('div').find('.icon-atv-error').removeClass('hidden');
+                    }
+                });
+
+                $('.input-soma1').each(function(){
+                    var num1 = $(this).attr('id');
+                    if($(this).val() == num1){
+                        $("#acertos-3").val(acertos++ + 1);
+                        $(this).addClass('no-click');
+                        $(this).attr('readonly', 'true');
+                        $(this).unbind('click');
+                        $(this).addClass('acerto');
+                        $(this).parent('div').prev('div').find('.icon-atv-error').addClass('hidden');
+                        $(this).parent('div').prev('div').find('.icon-atv-check').removeClass('hidden');
+                    } else if($(this).val() != num1){
+                        $("#erros-3").val(erros++ + 1);
+                        $(this).addClass('erro');
+                        $(this).parent('div').prev('div').find('.icon-atv-check').addClass('hidden');
+                        $(this).parent('div').prev('div').find('.icon-atv-error').removeClass('hidden');
+                    }
+                });
+
+                $('.input-soma2').each(function(){
+                    var num2 = $(this).attr('id');
+                    if($(this).val() == num2){
+                        $("#acertos-3").val(acertos++ + 1);
+                        $(this).addClass('no-click');
+                        $(this).attr('readonly', 'true');
+                        $(this).unbind('click');
+                        $(this).addClass('acerto');
+                        $(this).parent('div').prev('div').find('.icon-atv-error').addClass('hidden');
+                        $(this).parent('div').prev('div').find('.icon-atv-check').removeClass('hidden');
+                    } else if($(this).val() != num2){
+                        $("#erros-3").val(erros++ + 1);
+                        $(this).addClass('erro');
+                        $(this).parent('div').prev('div').find('.icon-atv-check').addClass('hidden');
+                        $(this).parent('div').prev('div').find('.icon-atv-error').removeClass('hidden');
+                    }
+                });
+            } else if (acertos >= 12){
+                swal("Parabéns, você acertou todas as respostas!", "", "success");
+                playSound('claps');
+
+                var sbtvId = $("#hdn_subatividade_id-3").val();
+                var toHide = '';
+                var toShow = '';
+                var erros = $("#erros-3").val();
+                var acertos = $("#acertos-3").val();
+                var dateFim = getDate();
+                var no_errors = false;
+
+                if(erros == 0){
+                    no_errors = true;
+                }
+
+                minTry(sbtvId, 6, acertos, erros, toHide, toShow, 3, dateIni, dateFim, no_errors);
             }
-
-            minTry(sbtvId, 6, acertos, erros, toHide, toShow, 3, dateIni, dateFim, no_errors);
         });
 
         $('#btn-prev-3').on('click', function () {

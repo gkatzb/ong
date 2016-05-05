98a4ee621110a4531a1bf69d668f393012ee8754
thkone <katz.gabriela@gmail.com>
2016-05-05 00:03:49 -0300

    complete_a

--- resources/views/includes/portugues/complete_a.blade.php
+++ resources/views/includes/portugues/complete_a.blade.php
@@ -13,22 +13,22 @@
                             <input type="text" class="task_input" id="A" maxlength="1" />
                         </td>
                         <td class="text-center">
-                            <input type="text" class="task_input" id="B" disabled readonly value="B" maxlength="1" />
+                            <input type="text" class="task_input" id="B"  readonly value="B" maxlength="1" />
                         </td>
                         <td>
                             <input type="text" class="task_input" id="A" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" value="C" disabled readonly id="C" maxlength="1" />
+                            <input type="text" class="task_input" value="C"  readonly id="C" maxlength="1" />
                         </td>
                         <td>
                             <input type="text" class="task_input" id="A" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" value="X" disabled readonly id="X" maxlength="1" />
+                            <input type="text" class="task_input" value="X"  readonly id="X" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" disabled readonly value="I" id="I" maxlength="1" />
+                            <input type="text" class="task_input"  readonly value="I" id="I" maxlength="1" />
                         </td>
                     </tr>
                     <tr>
@@ -66,22 +66,22 @@
                             <input type="text" class="task_input" id="A" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" value="B" disabled readonly id="B" maxlength="1" />
+                            <input type="text" class="task_input" value="B"  readonly id="B" maxlength="1" />
                         </td>
                         <td>
                             <input type="text" class="task_input" id="A" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" value="C" disabled readonly id="C" maxlength="1" />
+                            <input type="text" class="task_input" value="C"  readonly id="C" maxlength="1" />
                         </td>
                         <td>
                             <input type="text" class="task_input" id="A" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" disabled readonly value="T" id="T" maxlength="1" />
+                            <input type="text" class="task_input"  readonly value="T" id="T" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" disabled readonly value="E" id="E" maxlength="1" />
+                            <input type="text" class="task_input"  readonly value="E" id="E" maxlength="1" />
                         </td>
                     </tr>
                     <tr>
@@ -135,13 +135,13 @@
                             <input type="text" class="task_input" id="A" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" disabled readonly value="N" id="N" maxlength="1" />
+                            <input type="text" class="task_input"  readonly value="N" id="N" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" disabled readonly value="E" id="E" maxlength="1" />
+                            <input type="text" class="task_input"  readonly value="E" id="E" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" disabled readonly value="L" id="L" maxlength="1" />
+                            <input type="text" class="task_input"  readonly value="L" id="L" maxlength="1" />
                         </td>
                     </tr>
                     <tr>
@@ -170,16 +170,16 @@
                             <input type="text" class="task_input" id="A" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" value="B" disabled readonly id="B" maxlength="1" />
+                            <input type="text" class="task_input" value="B"  readonly id="B" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" disabled readonly value="E" id="E" maxlength="1" />
+                            <input type="text" class="task_input"  readonly value="E" id="E" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" value="L" disabled readonly id="L" maxlength="1" />
+                            <input type="text" class="task_input" value="L"  readonly id="L" maxlength="1" />
                         </td>
                         <td>
-                            <input type="text" class="task_input" value="H" disabled readonly id="H" maxlength="1" />
+                            <input type="text" class="task_input" value="H"  readonly id="H" maxlength="1" />
                         </td>
                         <td>
                             <input type="text" class="task_input" id="A" maxlength="1" />
@@ -229,32 +229,11 @@
         var $dateIni = getDate();
 
         $('.btn-atividade-9').on('click', function(e){
-            if($acertos != 7) {
+            if($acertos < 33) {
                 swal("Ops... Alguma resposta está errada!", "", "warning");
                 playSound('wrong');
-
-                $('.task_input').each(function(){
-                    var letra = $(this).attr('id');
-                    if($(this).val().toUpperCase() == letra.toUpperCase()){
-                        if(!$(this).hasClass('acerto-tbl')){
-                            $("#acertos-9").val($acertos++);
-                        }
-                        $(this).addClass('no-click');
-                        $(this).attr('readonly', 'true');
-                        $(this).unbind('click');
-                        $(this).addClass('acerto-tbl');
-                        if(!$(this).parent('table').find('.task_input').hasClass('erro-tbl')){
-                            $(this).parent('table').parent('div').find('.icon-atv-error').addClass('hidden');
-                            $(this).parent('table').parent('div').find('.icon-atv-check').removeClass('hidden');
-                        }
-                    } else if($(this).val().toUpperCase() != letra.toUpperCase()){
-                        $("#erros-9").val($erros++);
-                        $(this).addClass('erro-tbl');
-                        $(this).parent('table').parent('div').find('.icon-atv-check').addClass('hidden');
-                        $(this).parent('table').parent('div').find('.icon-atv-error').removeClass('hidden');
-                    }
-                });
-            } else if ($acertos >= 7){
+            }
+            else if ($acertos >= 33){
                 playSound('claps');
                 $(e).addClass('no-click');
                 $(e).attr('readonly', 'true');
@@ -263,28 +242,62 @@
                 $(e).parent('table').parent('div').find('.icon-atv-error').addClass('hidden');
                 $(e).parent('table').parent('div').find('.icon-atv-check').removeClass('hidden');
                 swal({
-                            title: "Parabéns, você acertou todas as respostas!",
-                            type: "success",
-                            showCancelButton: false,
-                            closeOnConfirm: true
-                        },
-                        function (isConfirm) {
-                            if (isConfirm) {
-                                var sbtvId = $("#hdn_subatividade_id-9").val();
-                                var toHide = '.subatividade-9';
-                                var toShow = '.subatividade-10';
-                                var erros = $("#erros-9").val();
-                                var acertos = $("#acertos-9").val();
-                                var dateFim = getDate();
-                                var no_errors = false;
+                    title: "Parabéns, você acertou todas as respostas!",
+                    type: "success",
+                    showCancelButton: false,
+                    closeOnConfirm: true
+                    },
+                    function (isConfirm) {
+                        if (isConfirm) {
+                            var sbtvId = $("#hdn_subatividade_id-9").val();
+                            var toHide = '.subatividade-9';
+                            var toShow = '.subatividade-10';
+                            var erros = $("#erros-9").val();
+                            var acertos = $("#acertos-9").val();
+                            var dateFim = getDate();
+                            var no_errors = false;
 
-                                if(erros == 0){
-                                    no_errors = true;
-                                }
-
-                                minTry(sbtvId, 8, acertos, erros, toHide, toShow, 9, $dateIni, dateFim, no_errors);
+                            if(erros == 0){
+                                no_errors = true;
                             }
-                        });
+
+                            minTry(sbtvId, 32, acertos, erros, toHide, toShow, 9, $dateIni, dateFim, no_errors);
+                        }
+                });
+            }
+        });
+
+        $('.task_input').on('keyup', function(e){
+            if(!$(this).is('[readonly]')){
+                var letra = $(this).attr('id');
+                if($(this).val().toUpperCase() == letra){
+                    if(!$(this).hasClass('acerto-tbl')){
+                        $("#acertos-9").val($acertos++);
+                    }
+                    $(this).addClass('no-click');
+                    $(this).attr('readonly', 'true');
+                    $(this).unbind('click');
+                    $(this).addClass('acerto-tbl');
+                    if(!$(this).parent('table').find('.task_input').hasClass('erro-tbl')){
+                        $(this).parent('table').parent('div').find('.icon-atv-error').addClass('hidden');
+                        $(this).parent('table').parent('div').find('.icon-atv-check').removeClass('hidden');
+                    }
+                    playSound('claps');
+                    swal({
+                                title: "Parabéns, a resposta está correta!",
+                                type: "success",
+                                showCancelButton: false,
+                                closeOnConfirm: true
+                            });
+
+                } else if($(this).val().toUpperCase() != letra){
+                    swal("Ops... Resposta errada!", "", "warning");
+                    playSound('wrong');
+                    $("#erros-9").val($erros++);
+                    $(this).addClass('erro-tbl');
+                    $(this).parent('table').parent('div').find('.icon-atv-check').addClass('hidden');
+                    $(this).parent('table').parent('div').find('.icon-atv-error').removeClass('hidden');
+                }
             }
         });
 

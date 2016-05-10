c74a2d81813668d2203d306bffef468cefb366e7
thkone <katz.gabriela@gmail.com>
2016-05-10 07:56:01 -0300

    adjusts

--- resources/views/includes/portugues/substantivos.blade.php
+++ resources/views/includes/portugues/substantivos.blade.php
@@ -16,52 +16,52 @@
                     <th>SUBSTANTIVOS PRÓPRIOS</th>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
                 <tr>
-                    <td><input class="comum input-plur" id="23" /></td>
-                    <td><input class="proprio input-plur" id="23" /></td>
+                    <td><input class="comum input-subs" id="23" /></td>
+                    <td><input class="proprio input-subs" id="23" /></td>
                 </tr>
             </table>
                 <i class="hidden icon-gen-check icon-gen-check-4 fa fa-check fa-3x margin-bottom"></i>
@@ -77,26 +77,31 @@
         var erros = 0;
         var acertos = 0;
         var dateIni = getDate();
-        var arrComum = [ATLÂNTICO &nbsp;DIAMANTINA &nbsp;PEDRO &nbsp;ÁGUA &nbsp;MARIANA &nbsp;BAHIA<br/>
-        URANO &nbsp;ABELHA &nbsp;NILO &nbsp;ESPADA &nbsp;CARLOS &nbsp;SAPATO<br/>
-        PARAGUAI &nbsp;RORAIMA &nbsp;BERMUDA &nbsp;BOLA &nbsp;RUA &nbsp;PESSOA<br/>
-        VERÃO &nbsp;CHUVA &nbsp;BELÉM &nbsp;AMOR &nbsp;GLOBO &nbsp;MARAJÓ<br/>];
-        var arrProprio = [];
+        var arrComum = ['ÁGUA', 'URANO', 'ABELHA', 'ESPADA', 'SAPATO', 'BERMUDA', 'BOLA', 'RUA', 'PESSOA', 'CHUVA',
+            'AMOR', 'GLOBO'];
+        var arrProprio = ['PARAGUAI', 'RORAIMA', 'ATLÂNTICO', 'DIAMANTINA', 'PEDRO', 'MARIANA', 'BAHIA', 'CARLOS',
+            'VERÃO', 'NILO', 'BELÉM', 'MARAJÓ'];
 
         $('.comum').on('blur', function () {
-            if ($(this).val() == $(this).val()) {
-                if(acertos < 24){
-                    $("#acertos-14").val(acertos++ + 1);
+            var isCorrect = false;
+            var palavra = $(this).val().toUpperCase();
+            for (var i = 0; i <= arrComum.length; i++) {
+                if (palavra == arrComum[i]) {
+                    if (acertos < 11) {
+                        $("#acertos-17").val(acertos++ + 1);
+                    }
+                    swal("Parabéns, resposta certa!", "", "success");
+                    playSound('claps');
+                    $(this).addClass('acerto');
+                    $(this).next('.icon-gen-check').removeClass('hidden');
+                    $(this).next('.icon-gen-check').next('.icon-gen-error').addClass('hidden');
+                    $(this).unbind('click');
+                    $(this).attr('disabled', true);
+                    isCorrect = true;
                 }
-                swal("Parabéns, resposta certa!", "", "success");
-                playSound('claps');
-                $(this).addClass('acerto');
-                $(this).next('.icon-gen-check').removeClass('hidden');
-                $(this).next('.icon-gen-check').next('.icon-gen-error').addClass('hidden');
-                $(this).unbind('click');
-                $(this).attr('disabled', true);
-            } else {
-                $("#erros-14").val(erros++ + 1);
+            }
+            if(!isCorrect) {
+                $("#erros-17").val(erros++ + 1);
                 swal("Ops... Resposta errada!", "", "warning");
                 playSound('wrong');
                 $(this).next('.icon-gen-check').next('.icon-gen-error').removeClass('hidden');
@@ -104,12 +109,39 @@
             }
         });
 
-        $('.btn-atividade-14').on('click', function (e) {
-            var sbtvId = $("#hdn_subatividade_id-14").val();
+        $('.proprio').on('blur', function () {
+            var isCorrect = false;
+            var palavra = $(this).val().toUpperCase();
+            for (var i = 0; i <= arrProprio.length; i++) {
+                if (palavra == arrProprio[i]) {
+                    if (acertos < 11) {
+                        $("#acertos-17").val(acertos++ + 1);
+                    }
+                    swal("Parabéns, resposta certa!", "", "success");
+                    playSound('claps');
+                    $(this).addClass('acerto');
+                    $(this).next('.icon-gen-check').removeClass('hidden');
+                    $(this).next('.icon-gen-check').next('.icon-gen-error').addClass('hidden');
+                    $(this).unbind('click');
+                    $(this).attr('disabled', true);
+                    isCorrect = true;
+                }
+            }
+            if(!isCorrect) {
+                $("#erros-17").val(erros++ + 1);
+                swal("Ops... Resposta errada!", "", "warning");
+                playSound('wrong');
+                $(this).next('.icon-gen-check').next('.icon-gen-error').removeClass('hidden');
+                $(this).addClass('erro');
+            }
+        });
+
+        $('.btn-atividade-17').on('click', function (e) {
+            var sbtvId = $("#hdn_subatividade_id-17").val();
             var toHide = '';
             var toShow = '';
-            var erros = $("#erros-14").val();
-            var acertos = $("#acertos-14").val();
+            var erros = $("#erros-17").val();
+            var acertos = $("#acertos-17").val();
             var dateFim = getDate();
             var no_errors = false;
 
@@ -117,11 +149,11 @@
                 no_errors = true;
             }
 
-            minTry(sbtvId, 24, acertos, erros, toHide, toShow, 6, dateIni, dateFim, no_errors);
+            minTry(sbtvId, 24, acertos, erros, toHide, toShow, 8, dateIni, dateFim, no_errors);
         });
 
         $('#btn-prev-8').on('click', function () {
-            var toHide = '.subatividade-14';
+            var toHide = '.subatividade-17';
             var toShow = '';
             btnPrevAtvd(toHide, toShow);
         });
